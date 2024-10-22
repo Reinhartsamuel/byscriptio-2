19f3181d507f1fe78be11261ef033f6bfa08a675
Reinhart Samuel <106574869+Reinhartsamuel@users.noreply.github.com>
2024-10-23 00:33:56 +0700

    validate signature formulation

--- app/api/3commas/accounts/user-connected-exchanges/route.js
+++ app/api/3commas/accounts/user-connected-exchanges/route.js
@@ -3,16 +3,18 @@ import HmacSHA256 from 'crypto-js/hmac-sha256';
 import Hex from 'crypto-js/enc-hex';
 // import CryptoJS from 'crypto-js';
 
-const three_commas_url = process.env.THREE_COMMAS_URL;
-const three_commas_api_key = process.env.THREE_COMMAS_API_KEY;
-const three_commas_api_secret = process.env.THREE_COMMAS_API_SECRET;
-
 const signature = (secret, url, params) => {
+  if (!secret || !url) {
+    return 'invalid credentials';
+  }
   const message = params ? `${url}?${params}` : url;
   return HmacSHA256(message, secret).toString(Hex);
 };
 
 export async function GET() {
+  const three_commas_url = process.env.THREE_COMMAS_URL;
+  const three_commas_api_key = process.env.THREE_COMMAS_API_KEY;
+  const three_commas_api_secret = process.env.THREE_COMMAS_API_SECRET;
   const queryString = '/ver1/accounts/';
   //   const signature = createSignature(three_commas_api_secret, 'https://api.3commas.io/public/api', '/ver1/accounts/');
   const headers = {
