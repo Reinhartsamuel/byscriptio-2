e1327e8d36fbb45184c651c4e040083f5290edfc
Reinhart Samuel <106574869+Reinhartsamuel@users.noreply.github.com>
2024-10-23 00:31:54 +0700

    test deploy sigbytes error

--- app/api/3commas/accounts/user-connected-exchanges/route.js
+++ app/api/3commas/accounts/user-connected-exchanges/route.js
@@ -8,31 +8,39 @@ const three_commas_api_key = process.env.THREE_COMMAS_API_KEY;
 const three_commas_api_secret = process.env.THREE_COMMAS_API_SECRET;
 
 const signature = (secret, url, params) => {
-    const message = params ? `${url}?${params}` : url;
-    return HmacSHA256(message, secret).toString(Hex)
-  }
+  const message = params ? `${url}?${params}` : url;
+  return HmacSHA256(message, secret).toString(Hex);
+};
 
 export async function GET() {
   const queryString = '/ver1/accounts/';
-//   const signature = createSignature(three_commas_api_secret, 'https://api.3commas.io/public/api', '/ver1/accounts/');
+  //   const signature = createSignature(three_commas_api_secret, 'https://api.3commas.io/public/api', '/ver1/accounts/');
   const headers = {
     Apikey: three_commas_api_key,
-    Signature: signature(three_commas_api_secret, three_commas_url, queryString),
+    Signature: signature(
+      three_commas_api_secret,
+      three_commas_url,
+      queryString
+    ),
   };
   console.log(headers.Signature);
 
-//   return Response.json({
-//     three_commas_url,
-//     three_commas_api_key,
-//     three_commas_api_secret,
-//     signature : headers.Signature
-//   });
+  //   return Response.json({
+  //     three_commas_url,
+  //     three_commas_api_key,
+  //     three_commas_api_secret,
+  //     signature : headers.Signature
+  //   });
 
   try {
     const res = await fetch(three_commas_url + queryString, {
       method: 'GET',
       headers: {
-        Signature:signature(three_commas_api_secret, three_commas_url, queryString),
+        Signature: signature(
+          three_commas_api_secret,
+          three_commas_url,
+          queryString
+        ),
         Apikey: three_commas_api_key,
       },
     });
