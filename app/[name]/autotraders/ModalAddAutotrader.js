870a83123a9f52d3250fd093857961600533f59c
Reinhart Samuel <106574869+Reinhartsamuel@users.noreply.github.com>
2024-11-12 15:43:43 +0700

    handlesubmit add autotrader

--- app/[name]/autotraders/ModalAddAutotrader.js
+++ app/[name]/autotraders/ModalAddAutotrader.js
@@ -42,7 +42,7 @@ export default function ModalAddAutotrader({ addModal, setAddModal }) {
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = async () => {
-    if (!userPackage) throw new Error("You don't have an active package");
+    // if (!userPackage) throw new Error("You don't have an active package");
     setLoading(false);
     if (!data?.exchange_name || !data?.exchange_thumbnail)
       return Swal.fire({ icon: 'warning', text: 'Please select exchange!' });
