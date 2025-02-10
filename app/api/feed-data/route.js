f2828b9c41edb9b16a6af8a6f5562501ac186d9a
Reinhart Samuel <smuel.rr@gmail.com>
2025-02-11 00:19:43 +0700

    add data to data_feed

---
+++ app/api/feed-data/route.js
@@ -0,0 +1,22 @@
+import { adminDb } from "@/lib/firebase-admin-config";
+
+export async function POST (request) {
+    try {
+        const body = await request.json();
+        const { data } = body;
+
+        const result = await Promise.allSettled(
+            data?.map(async (x) => {
+                await adminDb.collection('data_feed').add({
+                    ...x,
+                })
+        }))
+        return Response.json({
+            status : true,
+            message : 'success',
+            result : result.map((x) => x.status === 'fulfilled' ? x.value : x.reason),
+        });
+    } catch (error) {
+        return Response.json({ status : false, message : error.message});
+    }
+}
