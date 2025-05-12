ff7a37a01d9920ef190faecc1a08b4abfa8b5ba4
reynhartsamuel <rr.smuel@gmail.com>
2025-05-12 17:51:35 +0700

    add stringified body to signature calculation

--- app/api/playground/3commas/route.js
+++ app/api/playground/3commas/route.js
@@ -25,50 +25,57 @@ export const maxDuration = 60; // This function can run for a maximum of 60 seco
 
 export async function POST(request) {
     try {
-      const body = await request.json();
-      console.log(body, 'body')
-    const totalParams = `/public/api` + body.queryParams;
-    const finalUrl = baseUrl + totalParams;
-    const signature = generateSignatureRsa(PRIVATE_KEY, totalParams);
-    const response = await fetch(finalUrl, {
-        method: body.method,
-        body: body.bodySend ? JSON.stringify(body.bodySend) : null,
-        headers: {
-            'Content-Type': 'application/json',
-            APIKEY: API_KEY,
-            Signature: signature,
+        const body = await request.json();
+        console.log(body, 'body')
+        let totalParams = `/public/api` + body.queryParams;
+        if (body?.bodySend && Object.keys(body?.bodySend).length > 0) {
+            totalParams += JSON.stringify(body.bodySend)
         }
-    });
-    const data = await response.json();
-    if (!response.ok) {
-        console.log(data,'data... response not ok')
+        const finalUrl = baseUrl + totalParams;
+        const signature = generateSignatureRsa(PRIVATE_KEY, totalParams);
+        const response = await fetch(finalUrl, {
+            method: body.method,
+            body: body.bodySend ? JSON.stringify(body.bodySend) : null,
+            headers: {
+                'Content-Type': 'application/json',
+                APIKEY: API_KEY,
+                Signature: signature,
+            }
+        });
+        const data = await response.json();
+        if (!response.ok) {
+            console.log(data, 'data... response not ok')
+            const returnErrorData = {
+                status: false,
+                error: data.error + ' ' + data?.error_attributes,
+                message: data.error,
+                error_attributes: data?.error_attributes,
+                errorCode: data.code,
+                error_description: data?.error_description
+            }
+            if (data.error === 'signature_invalid') {
+                returnErrorData.calculatedSignature = signature;
+            }
+            return new Response(JSON.stringify(returnErrorData), {
+                status: 400,
+                headers: { 'Content-Type': 'application/json' }
+            })
+        }
+        return new Response(JSON.stringify({
+            status: true,
+            data: data
+        }), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' }
+        })
+    } catch (error) {
         return new Response(JSON.stringify({
             status: false,
-            error: data.error + ' ' + data?.error_attributes,
-            message: data.error,
-            error_attributes : data?.error_attributes,
-            errorCode: data.code,
-            error_description: data?.error_description
+            message: error.message,
+            errorCode: error.code
         }), {
-            status: 400,
+            status: 500,
             headers: { 'Content-Type': 'application/json' }
         })
     }
-    return new Response(JSON.stringify({
-        status: true,
-        data : data
-    }), {
-        status: 200,
-        headers: { 'Content-Type': 'application/json' }
-    })
-} catch (error) {
-    return new Response(JSON.stringify({
-        status: false,
-        message: error.message,
-        errorCode: error.code
-    }), {
-        status: 500,
-        headers: { 'Content-Type': 'application/json' }
-    })
-}
 }
