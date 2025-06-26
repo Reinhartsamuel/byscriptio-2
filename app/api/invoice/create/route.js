f04ef84450f6037a9f71e072ce47be6d76f52193
reynhartsamuel <rr.smuel@gmail.com>
2025-06-26 22:15:39 +0700

    chore: add logging to body create invoice

--- app/api/invoice/create/route.js
+++ app/api/invoice/create/route.js
@@ -2,24 +2,26 @@ export async function POST(request) {
     try {
         const body = await request.json();
         const { customer, price, subscriptionId, productName } = body;
+        const postData = {
+            "price_amount": price,
+            "price_currency": "idr",
+            "order_id": subscriptionId,
+            "order_description": `${customer?.name} - ${customer?.email} - ${productName}`,
+            "ipn_callback_url": "https://www.byscript.io/api/callback",
+            "success_url": `https://byscript.io/${customer?.name}`,
+            "cancel_url": `https://byscript.io/${customer?.name}`,
+            "partially_paid_url": "https://nowpayments.io",
+            "is_fixed_rate": true,
+            "is_fee_paid_by_user": false
+        };
+        console.log('postData', postData);
         const r = await fetch('https://api.nowpayments.io/v1/invoice', {
             method: 'POST',
             headers: {
                 'x-api-key': process.env.NOWPAYMENTS_API_KEY,
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({
-                "price_amount": price,
-                "price_currency": "idr",
-                "order_id": subscriptionId,
-                "order_description": `${customer?.name} - ${customer?.email} - ${productName}`,
-                "ipn_callback_url": "https://www.byscript.io/api/callback",
-                "success_url": `https://byscript.io/${customer?.name}`,
-                "cancel_url": `https://byscript.io/${customer?.name}`,
-                "partially_paid_url": "https://nowpayments.io",
-                "is_fixed_rate": true,
-                "is_fee_paid_by_user": false
-            })
+            body: JSON.stringify(postData)
         });
         const paymentResponse = await r.json();
         if (r.status !== 200) {
@@ -37,7 +39,7 @@ export async function POST(request) {
         return new Response(JSON.stringify({
             status: false,
             message: error.message,
-            error:'internal server error!'
+            error: 'internal server error!'
         }), {
             status: 500
         })
