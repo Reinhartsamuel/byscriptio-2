7cd737dc8825214c0f0b86f9d656dffa5e7fc240
reynhartsamuel <rr.smuel@gmail.com>
2025-07-24 13:18:11 +0700

    fix: change /[name] to /dashboard

--- app/components/SignInFrontButton.js
+++ app/components/SignInFrontButton.js
@@ -7,7 +7,7 @@ const SignInFrontButton = () => {
     return (
         <>
             {customer ?
-                <a href={`/${customer.name}`}>
+                <a href='/dashboard'>
                     <button className='w-[90%] p-[3px] relative my-5'>
                         <div className='absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg' />
                         <div className='px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white bg-transparent active:bg-violet-700'>
